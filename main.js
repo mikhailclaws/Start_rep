a7480d3c43ce6c4b78dd958c6e5fe1f7e768eb67
Mikhailclaws <mikhailclaws@gmail.com>
2022-08-14 18:44:15 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+
+let a = 1;
+let b = 8;
+let c = 16;
+let d = a+b+c;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(a+b+c);
+console.log(d+100);
+console.log(d+"Вася");
+
+let introduce ={ lol:"Мама", kek:"я", cheburek: "программист"}
+console.log(introduce)
+
+
+let admin;
+let username = "Джон";
+admin = username
+
+alert(username)
+console.log(username)
